ab6b2ae0318e69ffde1e65d5027d5a84d34df16e
Eduard Philip <eduardophilip95@gmail.com>
2021-11-03 21:49:35 +0000

    connection to database

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const colors = require('colors')
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 
 app.get('/', (req, res) => {
@@ -11,4 +14,10 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server runnig in ${process.env.PORT}`.yellow.bold))
+app.listen(PORT, console.log(`Server runnig in ${process.env.PORT}`.yellow.bold));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+  });
