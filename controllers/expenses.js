e0c7b4b1a55cd2238fac70deffc330396d6c0b23
Eduard Philip <eduardophilip95@gmail.com>
2021-11-08 07:50:16 +0000

    method to get all and single expense

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -3,15 +3,47 @@ const Expense = require('../models/Expense')
 // @desc      Get all expenses
 // @route     GET /api/v1/expenses
 // @access    Public
-exports.getExpenses = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Show all expenses'});
+exports.getExpenses = async (req, res, next) => {
+    
+    try {
+        const expense = await Expense.find();
+    
+        res.status(200).json({
+            success: true,
+            count: expense.length,
+            data: expense
+        });
+    } catch(err) {
+        res.status(400).json({success: false});
+        console.log(err);
+    }
+
 }
 
 // @desc      Get single expense
 // @route     GET /api/v1/expenses
 // @access    Public
-exports.getExpense = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Show single expense'});
+exports.getExpense = async (req, res, next) => {
+    
+    try {
+        const expense = await Expense.findById(req.params.id);
+
+        if (!expense) {
+            res.status(400).json({
+                success: false,
+                msg: 'Expense not found'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: expense
+        }); 
+    } catch {
+        res.status(400).json({success: false});
+        console.log(err);
+    }
+
 }
 
 // @desc      Add new expense
