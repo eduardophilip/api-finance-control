c1e90cb86194887065821ca9526610c976c1d0b6
Eduard Philip <eduardophilip95@gmail.com>
2021-11-17 00:15:40 +0000

    Logout user

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const { 
     register,
-    login
+    login,
+    logout
  } = require('../controllers/auth.controller');
 const router = express.Router();
 
+const { protect } = require('../middleware/auth.middleware');
+
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', protect, logout);
 
 
 module.exports = router;
